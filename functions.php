d62bb4c49223e4afdcf6a9dbdc7974d196d3ddc4
Talia <talia.j.degisi@gmail.com>
2023-08-29 00:37:01 -0400

    Tweaks (including presentation of post edit date)

--- functions.php
+++ functions.php
@@ -133,6 +133,59 @@ function understrap_post_nav() {
 
 
 /**
+ * Change presentation of date a post is edited
+ */
+function understrap_posted_on() {
+	$post = get_post();
+	if ( ! $post ) {
+		return;
+	}
+
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> <i>(edited %4$s)</i></time>';
+	}
+
+	$time_string = sprintf(
+		$time_string,
+		esc_attr( get_the_date( 'c' ) ), // @phpstan-ignore-line -- post exists
+		esc_html( get_the_date() ), // @phpstan-ignore-line -- post exists
+		esc_attr( get_the_modified_date( 'c' ) ), // @phpstan-ignore-line -- post exists
+		esc_html( get_the_modified_date() ) // @phpstan-ignore-line -- post exists
+	);
+
+	$posted_on = apply_filters(
+		'understrap_posted_on',
+		sprintf(
+			'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
+			esc_html_x( 'Posted on', 'post date', 'understrap' ),
+			esc_url( get_permalink() ), // @phpstan-ignore-line -- post exists
+			apply_filters( 'understrap_posted_on_time', $time_string )
+		)
+	);
+
+	$author_id = (int) get_the_author_meta( 'ID' );
+	if ( 0 === $author_id ) {
+		$byline = '';
+	} else {
+		$byline = apply_filters(
+			'understrap_posted_by',
+			sprintf(
+				'<span class="byline"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
+				$posted_on ? esc_html_x( 'by', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
+				esc_url( get_author_posts_url( $author_id ) ),
+				esc_html( get_the_author_meta( 'display_name', $author_id ) )
+			)
+		);
+	}
+
+	echo $posted_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+}
+
+
+
+/**
  * Remove some attributes from featured images so they will display at full size.
  */
 function understrapsss_simplify_fimage($html) {
